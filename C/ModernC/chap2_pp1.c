8596c8d0239455f886903391d6efdcc47691b297
Aakash Karthik <k.aakash@outlook.com>
2026-01-15 16:47:48 +0530

    chap2 program project1(human readable format)

--- C/ModernC/chap2_pp1.c
+++ C/ModernC/chap2_pp1.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
-	printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
+	printf("       *\n");
+	printf("      *\n");
+	printf("     *\n");
+	printf("*   *\n");
+	printf(" * *\n");
+	printf("  *\n");
 	return 0;
 }
