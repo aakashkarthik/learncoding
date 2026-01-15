a83765e1f606be848c2e1735be517402dfaf7f45
Aakash Karthik <k.aakash@outlook.com>
2026-01-15 16:28:22 +0530

    chap2 ex4 modernC

---
+++ C/ModernC/chap2_ex4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y, z;
+	float a, b, c;
+	printf("x=%d y=%d z=%d a=%f b=%f c=%f\n",x ,y ,z ,a ,b ,c);
+	return 0;
+} //the integer value are random, but in float all are 0.0
