7f43b5a97210d27ae2b464d980c2b710bc58c0eb
Aakash Karthik <k.aakash@outlook.com>
2026-01-16 12:39:47 +0530

    chap2 program project5

---
+++ C/ModernC/chap2_pp5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	printf("give an integer value for x:");
+	scanf("%d",&x);
+	printf("then '3(x^5) + 2(x^4) - 5(x^3) - x^2 + 7x - 6' is:%d\n", 3*(x*x*x*x*x) + 2*(x*x*x*x) - 5*(x*x*x) - x*x + 7*x - 6);
+	return 0;
+}
