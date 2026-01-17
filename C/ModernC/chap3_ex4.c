d76beeeba914231167d569edefa7e851336e7e7b
Aakash Karthik <k.aakash@outlook.com>
2026-01-17 07:47:24 +0530

    chap3 exercise4

---
+++ C/ModernC/chap3_ex4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,j;
+	float x;
+	scanf("%d%f%d", &i, &x, &j);
+	printf("i = %d,x = %f,j = %d\n",i,x,j);/*in this program if the user enters '10.3 5 6' the result will be i = 10,x = 0.3,j = 5*/
+}
