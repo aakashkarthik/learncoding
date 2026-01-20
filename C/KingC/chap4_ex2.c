91e0cbd15a0cdea129d04dd0e3098170e6024e74
Aakash Karthik <k.aakash@outlook.com>
2026-01-20 18:01:57 +0530

    chap4 excercise2

--- C/KingC/chap4_ex2.c
+++ C/KingC/chap4_ex2.c
@@ -7,7 +7,7 @@ int main(void)
 	printf("enter two positive numbers for i and j(seperated by a comma): ");
 	scanf("%d,%d", &i, &j);
 	printf("(-i) / j is %d\n", (-i) / j);
-	printf("-(i / j) is %d\n", -(i / j));
+	printf("-(i / j) is %d\n", -(i / j));//both results are same and are always rounded towards the next number even if it is actually just 1 more.
 	
 	return 0;
 }
