b4e8792c4bcb51de4f82d0bf5c27450662da34ce
Aakash Karthik <k.aakash@outlook.com>
2026-01-31 12:41:45 +0530

    chap5 program project7 reduced comparisions

--- C/KingC/chap5_pp7.c
+++ C/KingC/chap5_pp7.c
@@ -6,24 +6,24 @@ int main(void)
 	printf("Enter 4 integers: ");
 	scanf("%d %d %d %d", &a, &b, &c, &d);
 	
-	if(a >= b && a >= c && a >= d)
-		largest = a;
-	else if(b >= a && b >= c && b >= d)
+	largest = a;
+	if(b > largest)
 		largest = b;
-	else if(c >= a && c >= b && c >= d)
+	if(c > largest)
 		largest = c;
-	else 
+	if(d > largest)
 		largest = d;
-	if(a <= b && a <= c && a <= d)
-		smallest = a;
-	else if(b <= a && b <= c && b <= d)
-		smallest = b;
-	else if(c <= b && c <= c && c <= d)
-		smallest = c;
-	else
-		smallest = d;
 	
 	printf("Largest: %d\n", largest);
+	
+	smallest = a;
+	if(b < smallest)
+		smallest = b;
+	if(c < smallest)
+		smallest = c;
+	if(d < smallest)
+		smallest = d;
+	
 	printf("smallest: %d\n", smallest);
 	
 	return 0;
