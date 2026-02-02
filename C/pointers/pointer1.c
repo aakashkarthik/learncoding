d2c736007214fd6b479a5f21b9fb528a8104b386
Aakash Karthik <k.aakash@outlook.com>
2026-02-02 15:45:58 +0530

    pointer1 version3

--- C/pointers/pointer1.c
+++ C/pointers/pointer1.c
@@ -7,8 +7,11 @@ int main(void)
 	
 	int *ptr = &a;
 	printf("The value of ptr: %p\n", ptr);
-	
 	printf("the value ptr is pointing to: %d\n", *ptr);//the second part of the program.
+	
+	char b = 'f';
+	char *ptr2 = &b;
+	printf("the value ptr2 is pointing to: %c\n", *ptr2);//the third part of the program.
 	
 	return 0;
 }
