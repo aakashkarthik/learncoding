d4c42ce984083c04aeaa6d0d95eb51196825aec0
Aakash Karthik <k.aakash@outlook.com>
2026-02-02 15:41:47 +0530

    pointer1 version2

--- C/pointers/pointer1.c
+++ C/pointers/pointer1.c
@@ -3,7 +3,12 @@
 int main(void)
 {
 	int a = 24;
-	printf("The value of a is: %d\n", a);
+	printf("The value of a is: %d\n", a);//first part of the program.
+	
+	int *ptr = &a;
+	printf("The value of ptr: %p\n", ptr);
+	
+	printf("the value ptr is pointing to: %d\n", *ptr);//the second part of the program.
 	
 	return 0;
 }
