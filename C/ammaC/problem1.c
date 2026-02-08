1fea3d6879cec17e34f50ed44d562e624c876d11
Aakash Karthik <k.aakash@outlook.com>
2026-02-08 12:48:55 +0530

    completed copy string reverse

---
+++ C/ammaC/problem1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* stringReverse(char *ptr, int size)
+{
+	char *ptr2 = (char*) malloc(sizeof(char) * size);
+	ptr2 += size - 1;
+	for(int k = 1; k <= size; k++)
+	{
+	*ptr = *ptr2;
+	ptr++;
+	}
+	
+	return ptr2;
+}
+
+int main(void)
+{
+	char *ch = (char*) malloc(10);
+	char *temp = ch;
+	
+	char c = 'a';
+	for(int j = 1; j < 10; j++, c++)
+	{
+		*temp = c;
+		*temp++;
+	}
+	
+	char *reverse = stringReverse(ch, 10);
+	for(int i = 0; i < 10; i++)
+	{
+	printf("%s", reverse);
+	reverse++;
+	}
+	printf("\n");
+	free(ch);
+	
+	return 0;
+}
