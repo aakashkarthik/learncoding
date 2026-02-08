bb9d18b1af73f8bba60b5836492ed96d1920969e
Aakash Karthik <k.aakash@outlook.com>
2026-02-08 18:32:01 +0530

    chap6 program project6

---
+++ C/KingC/chap6_pp6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	printf("This program calculates all the even squares from 1 to 'n'.\n\n");
+	printf("Enter the value for n: ");
+	scanf("%d", &n);
+	
+	for(int i = 2; i * i <= n; i++)
+	{
+		printf("%d\n", i * i);
+	}
+	printf("\n");
+	
+	return 0;
+}
