20712509b41bed1f9133eebd63bd7c67c3562619
Aakash Karthik <k.aakash@outlook.com>
2026-02-17 19:57:51 +0530

    chap7 exercise6

---
+++ C/KingC/chap7_ex6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+	printf("max char: %d\n", CHAR_MAX);
+	printf("max short: %d\n", SHRT_MAX);
+	printf("max int: %d\n", INT_MAX);
+	printf("max long: %ld\n", LONG_MAX);
+	//Therefore (a)char, (b)short, (c)short, (d)int; in the problem.
+}
