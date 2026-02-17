790e0dd6198001c0f813075299fc531c976366d4
Aakash Karthik <k.aakash@outlook.com>
2026-02-17 13:02:14 +0530

    chap7 exercise1

---
+++ C/KingC/chap7_ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	printf("The decimal value of the hexadecimal number 77: %d\n", 0x77);// it is 119.
+	printf("The decimal value of the hexadecimal number ABC: %d\n", 0xABC);// it is 2648.
+	printf("The decimal value of the octal number 77: %d\n", 077);// it is 63.
+	
+	return 0;
+}
