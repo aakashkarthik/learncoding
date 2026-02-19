2de446b11ac603f249b9d4b47050a60bba188f5e
Aakash Karthik <k.aakash@outlook.com>
2026-02-19 17:21:09 +0530

    added adjustments for excercise 6 and made a file with the size of some normal types

--- C/KingC/chap7_ex6.c
+++ C/KingC/chap7_ex6.c
@@ -7,5 +7,6 @@ int main(void)
 	printf("max short: %d\n", SHRT_MAX);
 	printf("max int: %d\n", INT_MAX);
 	printf("max long: %ld\n", LONG_MAX);
+    printf("size of:-\n");
 	//Therefore (a)char, (b)short, (c)short, (d)int; in the problem.
 }
---
+++ C/KingC/chap7_sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("char: %lu\n", sizeof(char));
+    printf("short: %lu\n", sizeof(short));
+    printf("int: %lu\n", sizeof(int));
+    printf("long: %lu\n", sizeof(long));
+    printf("long long: %lu\n", sizeof(long long));
+    printf("float: %lu\n", sizeof(float));
+    printf("double: %lu\n", sizeof(double));
+    printf("long double: %lu\n", sizeof(long double));
+}
