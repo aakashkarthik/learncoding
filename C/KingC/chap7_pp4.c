ac1df33264f40d0caf0fd48cc239a7f2137c5c48
Aakash Karthik <k.aakash@outlook.com>
2026-02-22 11:50:32 +0530

    chap7 program project4

---
+++ C/KingC/chap7_pp4.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int digit;
+    char c;
+    
+    printf("Enter phone number: ");
+    
+    c = getchar();
+    while(c != '\n')
+    {
+        switch(c)
+        {
+            case 'A': 
+            case 'B': 
+            case 'C': 
+                putchar('2');
+                break;
+            case 'D': 
+            case 'E': 
+            case 'F':
+                putchar('3');
+                break;
+            case 'G':
+            case 'H':
+            case 'I':
+                putchar('4');
+                break;
+            case 'J':
+            case 'K':
+            case 'L':
+                putchar('5');
+                break;
+            case 'M':
+            case 'N':
+            case 'O':
+                putchar('6');
+                break;
+            case 'P':
+            case 'R':
+            case 'S':
+                putchar('7');
+                break;
+            case 'T':
+            case 'U':
+            case 'V':
+                putchar('8');
+                break;
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
+                putchar('9');
+                break;
+            default:
+                putchar(c);
+                break;
+        }
+        
+        c = getchar();
+    }
+    printf("\n");
+    
+    return 0;
+}
