d8cf8a7a96d65ee3507926a1f5fb2618db4ff349
Aakash Karthik <k.aakash@outlook.com>
2026-03-13 18:23:35 +0530

    rewrote factorial function such that it is not recursive

---
+++ C/KingC/chap9/ex17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    int fact = 1;
+    for(int i = n; i > 1; i--)
+        fact *= i;
+    
+    return fact;
+}
+
+int main(void)
+{
+    printf("The factorial of %d is %d\n", 10, fact(10));
+    return 0;
+}
