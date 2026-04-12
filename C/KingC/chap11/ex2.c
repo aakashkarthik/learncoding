9207b462046d8327d92c35dd05222fe519273993
Aakash Karthik <k.aakash@outlook.com>
2026-04-12 19:57:10 +0530

    legal statement check

---
+++ C/KingC/chap11/ex2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int *p, *q;
+//    p = i; error
+//    *p = &i; error
+//    &p = q;  error
+//    p = &q; error
+//    p = *&q; no error
+//    p = q; no error
+//    p = *q; error
+//    *p = q; error
+//    *p = *q; no error
+}
